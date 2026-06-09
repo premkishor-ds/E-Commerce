c5fd6bac0d1526dbea29a29cfce8d274e1b7bca4
Prem Kishor - Dotsquares <102597355+premkishor-ds@users.noreply.github.com>
2026-06-09 10:41:59 +0530

    removed unused file

--- backend/test_cart.ts
+++
@@ -1,5 +0,0 @@
-import { SalesService } from './src/modules/sales/sales.service';
-import { Types } from 'mongoose';
-
-// Simple dry run logic to trace cart item quantity changes
-console.log('Cart Test Script');
